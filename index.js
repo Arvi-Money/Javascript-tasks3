46b6909d829d167973fd1fbb810d24a7d21fa130
sakiynam <mamatisayevasarvinoz@gmail.com>
2023-12-15 00:29:42 +0500

    Tasks

---
+++ index.js
@@ -0,0 +1,12 @@
+// 1
+// let n = 21;
+// let counter = 0;
+
+// for(let i = 1; i <= n; i++) {
+//     if(i / 3 == 0 || i / 7 == 0){
+//         counter += i;
+//     }
+// }
+
+// console.log(counter);
+
